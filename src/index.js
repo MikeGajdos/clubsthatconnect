b30aa5b11052510d13a8a1187b0cab502b35f79d
Mike Gajdos <mikegajdos81@gmail.com>
2021-08-12 11:11:20 +0100

    Removed comment in index.js

--- src/index.js
+++ src/index.js
@@ -33,26 +33,6 @@ results.forEach((splitResult) => {
   splitResult.el.innerHTML = wrappedLines;
 });
 
-// const scroller = new LocomotiveScroll({
-//   el: document.querySelector("[data-scroll-container]"),
-//   smooth: true,
-// });
-// scroller.on("scroll", ScrollTrigger.update);
-// ScrollTrigger.scrollerProxy(".container", {
-//   scrollTop(value) {
-//     return arguments.length
-//       ? scroller.scrollTo(value, 0, 0)
-//       : scroller.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {
-//       left: 0,
-//       top: 0,
-//       width: window.innerWidth,
-//       height: window.innerHeight,
-//     };
-//   },
-// });
 
 let selectedColorValue;
 let selectedCountry;
